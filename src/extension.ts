29aabb801b5a8ef7ffcfdc52f24110ddaf9ec0df
Hiromi ISHII <konn.jinro@gmail.com>
2018-11-12 02:17:12 +0900

    Adds autocompletion for unicode symbols with `generic-input-method`

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,36 @@
 "use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext } from "vscode";
-
+import { ExtensionContext, extensions } from "vscode";
 import RedttProvider from "./diagnostics";
+
+const REDTT_IM_NAME = "mr-konn.redtt-dignostics.im";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
+  const gim = extensions.getExtension("mr-konn.generic-input-method");
+  if (gim) {
+    console.log(`${gim.extensionPath}/types/api.d.ts`);
+    const api: any = await gim.activate();
+    const im: any = {
+      name: REDTT_IM_NAME,
+      languages: ["redtt"],
+      triggers: ["\\"],
+      dictionary: ["defaults/math.json", { label: "to", body: "→" }]
+    };
+    api.registerInputMethod(im);
+  } else {
+  }
   let provider: RedttProvider = new RedttProvider();
   context.subscriptions.push(provider);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  let gim = extensions.getExtension("mr-konn.generic-input-method");
+  if (gim) {
+    const api = gim.exports;
+    api.unregisterInputMethodByName(REDTT_IM_NAME);
+  }
+}
